ad7dbba9a50887c5050034c00aec78e28747c5f3
Said Ghamra <s.ghamra@hotmail.com>
2021-01-28 09:06:14 -0500

    Added error callback log for the proxy

--- src/modules/proxy/proxy.service.ts
+++ src/modules/proxy/proxy.service.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import * as HttpProxy from 'http-proxy';
 
@@ -14,23 +14,21 @@ export class ProxyService {
    * @param deploymentId the deployment id
    * @param req the request
    * @param res the response
-   * @param next the next function
    */
-  proxy(
-    deploymentId: string,
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ): void {
+  proxy(deploymentId: string, req: Request, res: Response): void {
     const options: HttpProxy.ServerOptions = {
       target: `http://${this.resourcePrefix}-${deploymentId}.${this.resourcePrefix}.svc.cluster.local`,
     };
     const connection: string = req.headers['connection'];
     const upgrade: string = req.headers['upgrade'];
     if (connection === 'Upgrade' && upgrade === 'websocket') {
-      this.httpProxy.ws(req, req.socket, req.app.head, options);
+      this.httpProxy.ws(req, req.socket, req.app.head, options, (err) => {
+        this.logger.error({ message: 'WS Proxy Error', err });
+      });
     } else {
-      this.httpProxy.web(req, res, options, next);
+      this.httpProxy.web(req, res, options, (err) => {
+        this.logger.error({ message: 'WEB Proxy Error', err });
+      });
     }
   }
 }
