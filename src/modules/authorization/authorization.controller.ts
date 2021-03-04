ef281c7657efc16c404e9a0c47c82ad90d443cc5
Said Ghamra <s.ghamra@hotmail.com>
2021-03-04 13:17:52 -0500

    #26 removed .isAdmin from user schena, working on authorization service

--- src/modules/authorization/authorization.controller.ts
+++ src/modules/authorization/authorization.controller.ts
@@ -1,21 +1,7 @@
-import { Controller, Get, Body, Put, Param } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AuthorizationService } from './authorization.service';
-import { UpdateAuthorizationDto } from './dto/update-authorization.dto';
 
 @Controller('api/authorization')
 export class AuthorizationController {
   constructor(private readonly authorizationService: AuthorizationService) {}
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.authorizationService.findOne(id);
-  // }
-
-  // @Put(':id')
-  // update(
-  //   @Param('id') id: string,
-  //   @Body() updateAuthorizationDto: UpdateAuthorizationDto,
-  // ) {
-  //   return this.authorizationService.update(id, updateAuthorizationDto);
-  // }
 }
