46d08d7bc179ed4d1377b69bdf3d6fb41ddd65c5
Said Ghamra <s.ghamra@hotmail.com>
2021-11-01 21:55:03 -0400

    fixed sign out issue

--- src/modules/authentication/authentication.service.ts
+++ src/modules/authentication/authentication.service.ts
@@ -114,14 +114,16 @@ export class AuthenticationService {
     if (user) {
       await this.clearRefreshToken(user.email);
     }
-    response.clearCookie(
-      this.accessCookieConfig.name,
-      this.accessCookieConfig.cookieOptions,
-    );
-    response.clearCookie(
-      this.refreshCookieConfig.name,
-      this.refreshCookieConfig.cookieOptions,
-    );
+    response.clearCookie(this.accessCookieConfig.name, {
+      ...this.accessCookieConfig.cookieOptions,
+      maxAge: this.accessCookieConfig.expirationTime * 1000,
+      domain: this.domain,
+    });
+    response.clearCookie(this.refreshCookieConfig.name, {
+      ...this.refreshCookieConfig.cookieOptions,
+      maxAge: this.refreshCookieConfig.expirationTime * 1000,
+      domain: this.domain,
+    });
   }
 
   /**
